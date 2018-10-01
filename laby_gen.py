b87d0611bc1ba6a7e690a0ec7e9b0d062cfd60fb
ZepmanBC <zepman@gmail.com>
2018-10-01 21:51:24 +0200

    add the last path, text at the bottom, red dots

--- laby_gen.py
+++ laby_gen.py
@@ -1,31 +1,44 @@
 #! /usr/bin/env python3
 # coding: utf-8
+
+"""
+librairie that generates a labyrinth in a list
+see MOVES for the formating
+
+"""
+from pprint import pprint
 import os
 import json
 from random import choice
 
 import numpy
 
-"""
-generer le labirynth
-faire un objet qui renvoi le labirynth sous forme de tableau
-avec un caractère hexa pour définir la cellule
-utiliser des labirynth prédéfini qui sont sauvegardé en json
-pour l'instant
-
-une classe qui ressort un tableau du labi
-
-classe de génération de labi
-on la défini par la taille x/y et ca nous renvoi un tableau 2 dimensions
-on l'appel labi_gen(x,*y)
-si il n'y a pas de y alors on ressort un labi existatne et sauvegardé
-dans un json
-"""
+# generer le labirynth
+# faire un objet qui renvoi le labirynth sous forme de tableau
+# avec un caractère hexa pour définir la cellule
+# utiliser des labirynth prédéfini qui sont sauvegardé en json
+# pour l'instant
+#
+# une classe qui ressort un tableau du labi
+#
+# classe de génération de labi
+# on la défini par la taille x/y et ca nous renvoi un tableau 2 dimensions
+# on l'appel labi_gen(x,*y)
+# si il n'y a pas de y alors on ressort un labi existatne et sauvegardé
+# dans un json
 
 
-class Laby_gen():
-
-    # { case : (Up,Right,Down,Left)}
+class LabyGenerator():
+    """
+        Generate a labyrinth
+        object = LabyGenerator()
+        object.generate(x, y) => return a list
+        ["123456",
+        "123456"]
+        each string is a line (x), each char is a case (y)
+        see MOVES for walls position
+    """
+    # { case : (Up, Right, Down, Left)}
     MOVES = {
         "0": (0, 0, 0, 0),
         "1": (0, 1, 0, 0),
@@ -50,10 +63,12 @@ class Laby_gen():
         if not os.path.exists(self.laby_data_file):
             file = open(self.laby_data_file, "w")
             json.dump({}, file)
+        self.path_record = []
 
     def generate(self, columns, rows):
         """
         generate a x/y laby
+        return : laby_code, longuest_path
         """
         # generate empty good dimension table
         cell_type = [1, 1, 1, 1, 0]  # North, Est, South, West, Visisted
@@ -63,12 +78,17 @@ class Laby_gen():
         pointer = (0, 0)
         path = []  # record the path
         path.append(pointer)
+        go_back = False
 
         while not self.test_all_visited(table):
             # return non visited possible cases around
             possible_cases = self.get_around_cases(pointer, table)
             if possible_cases == []:
                 # go back
+                if not go_back:
+                    self.path_record.append(path[:])
+                    # record a ended path
+                go_back = True
                 pointer = path.pop()
             else:
                 # choose a random direction not visited
@@ -76,15 +96,27 @@ class Laby_gen():
                 table = self.break_the_wall(pointer, go_to, table)
                 pointer = go_to
                 path.append(pointer)
-            print(pointer)
-        return self.translate_table(table)
+                go_back = False
+            # print(pointer)  #print the path
+        self.longuest_path()
+        return self.translate_table(table), self.longuest_path()
+
+    def longuest_path(self):
+        self.path_record.sort(key = len)
+        longuest = self.path_record[-1]
+        print(len(longuest))
+        print(longuest[-1])
+
+        # faire un test pour vérifier qu'il n'y aitpas d'incohérence dans la chaine
+
+        return longuest
 
     def get_around_cases(self, pointer, table):
         """
         get the non visited possible cases around the pointer
         """
         row_max, column_max = len(table), len(table[0])
-        all_cases = [self.go_north(pointer), self.go_est(pointer),
+        all_cases = [self.go_north(pointer), self.go_east(pointer),
                      self.go_south(pointer), self.go_west(pointer)]
         cases = []
         for case in all_cases:
@@ -96,7 +128,9 @@ class Laby_gen():
                     cases.append(case)
         return cases
 
-    def test_all_visited(self, table):
+    @staticmethod
+    def test_all_visited(table):
+        """ test if all cases have been 'visited' """
         for row in table:
             for column in row:
                 if column[4] == 0:
@@ -104,9 +138,7 @@ class Laby_gen():
         return True
 
     def break_the_wall(self, from_case, to_case, table):
-        """
-        make the wall disapear in the 2 visited cases
-        """
+        """make the wall disapear in the 2 visited cases"""
         # to the North
         if from_case[1] - to_case[1] == -1:
             table[(self.coord(from_case))][2] = 0
@@ -129,12 +161,12 @@ class Laby_gen():
         return table
 
     def translate_table(self, table):
+        """translate the table into the MOVES code"""
         translate_table = []
-        x_pos, y_pos = 0, 0
         for rows in table:
             translate_row = ""
             for case in rows:
-                for case_type in self.MOVES.keys():
+                for case_type in self.MOVES:
                     current_case = case[:4]
                     # need to numpy the tuple to compare same things
                     model_case = numpy.array(self.MOVES[case_type])
@@ -143,22 +175,31 @@ class Laby_gen():
             translate_table.append(translate_row)
         return translate_table
 
-    def coord(self, pointer):
+    @staticmethod
+    def coord(pointer):
         """
         invert pointer to be (x,y) coordonates
         """
         return (pointer[1], pointer[0])
 
-    def go_north(self, pointer):
+    @staticmethod
+    def go_north(pointer):
+        """return the pointer one case to the north"""
         return (pointer[0], pointer[1] - 1)
 
-    def go_est(self, pointer):
+    @staticmethod
+    def go_east(pointer):
+        """return the pointer one case to the east"""
         return (pointer[0] + 1, pointer[1])
 
-    def go_south(self, pointer):
+    @staticmethod
+    def go_south(pointer):
+        """return the pointer one case to the south"""
         return (pointer[0], pointer[1] + 1)
 
-    def go_west(self, pointer):
+    @staticmethod
+    def go_west(pointer):
+        """return the pointer one case to the west"""
         return (pointer[0] - 1, pointer[1])
 
     def getout(self, number):
@@ -173,12 +214,12 @@ class Laby_gen():
             print("laby number does not exist")
             return False
 
-    def save_laby(self, number, code):
+    def save_laby(self, number, code, longuest):
         """
         save a existing laby in a json file
         """
         data = self.read_saved_laby()
-        data[number] = code
+        data[number] = code, longuest
         file = open(self.laby_data_file, "r+")
         json.dump(data, file)
 
@@ -192,11 +233,12 @@ class Laby_gen():
 
 
 def main():
-    # os.remove("data/laby_data.json")
-    laby = Laby_gen()
-    code = laby.generate(20, 15)
-    laby.save_laby(4, code)
-    print(laby.getout(4))
+    """test function"""
+    os.remove("data/laby_data.json")
+    laby = LabyGenerator()
+    code, longuest = laby.generate(20, 15)
+    laby.save_laby(0, code, longuest)
+    print(laby.getout(0))
 
 if __name__ == "__main__":
     main()
--- laby_ui.py
+++ laby_ui.py
@@ -1,34 +1,44 @@
 #! /usr/bin/env python3
 # coding: utf-8
+
+"""
+generate the GUI for the labyrinth game
+init
+call menu_loop() => show menu
+call game_loop(labyrinth_code) => start a game
+"""
+
 import pygame
 from pygame.locals import *
 
 from laby_gen import *
 
-"""
-une representation graphique en utilisant pygame
-une classe qui s'occupe de la représentation graphique
-"""
+# une representation graphique en utilisant pygame
+# une classe qui s'occupe de la représentation graphique
 
 
-class laby_ui():
-
+class LabyGUI():
+    """ CLass that generate the GUI"""
     STEP = 30
 
     def __init__(self):
         pygame.init()
+        self.myfont = pygame.font.SysFont('Courier', 25, True)
         self.window = pygame.display.set_mode((640, 480))
 
     def game_loop(self, laby):
         """
         loop for a game laby
         """
-        self.laby = laby
-        pygame.draw.rect(self.window,
-                         pygame.Color('#FFFFFFFF'),
-                         (0, 0, 640, 480))
+        self.laby = laby[0]
+        self.longuest = laby[1]
+        self.last_case = self.longuest[-1]
+        move_left = len(self.longuest)
+        # pygame.draw.rect(self.window,
+        #                  pygame.Color('#FFFFFFFF'),
+        #                  (0, 0, 640, 480))
         self.load_laby(self.laby)
-        self.perso = Perso(self.window, self.STEP, self.laby)
+        self.perso = Perso(self.window, self.STEP, self.laby, move_left)
         pygame.display.flip()
 
         # pour rester appuyé sur le bouton
@@ -50,18 +60,45 @@ class laby_ui():
                         self.perso.right()
                     if event.key == K_LEFT:
                         self.perso.left()
-                pygame.draw.rect(self.window,
-                                 pygame.Color('#FFFFFFFF'),
-                                 (0, 0, 640, 480))
                 self.load_laby(self.laby)
-                self.perso.load()
-                # self.window.blit(perso, position_perso)
+                
+                
+                self.load_treasure(self.last_case)
+                move_left = self.perso.load()
+                self.bottom_text(move_left)
+                if move_left <= 0:
+                    perso_position = self.perso.return_position()
+                    if perso_position == self.last_case:
+                        self.end_party_text("You Win!!!")
+                    else:
+                        self.end_party_text("You Loose...")
+                    self.show_path(self.longuest)
+                    self.perso.load()
+
+                
                 pygame.display.flip()
 
+    def bottom_text(self, move_left):
+        label = self.myfont.render("moves left : {}".format(move_left),
+                                   0, (0,0,0))
+        self.window.blit(label, (10, 450))
+
+    def end_party_text(self, text):
+        label = self.myfont.render(text, 0, (0, 0, 0))
+        self.window.blit(label, (350, 450))
+
+
+    def load_treasure(self, case):
+        """show the treasure icon to the specified case"""
+        treasure = pygame.image.load("data/treasure.png").convert_alpha()
+        self.window.blit(treasure, self.coord(case))
+    
     def load_laby(self, code):
-        """
-        inside method for showing the laby
-        """
+        """inside method for showing the laby"""
+        # background => white for the moment
+        pygame.draw.rect(self.window,
+                         pygame.Color('#FFFFFFFF'),
+                         (0, 0, 640, 480))
         x_pos, y_pos = 0, 0
         # pygame.draw.rect(self.window,pygame.Color('#FFFFFFFF'),(0,0,640,480))
         for row in code:
@@ -71,6 +108,11 @@ class laby_ui():
                 self.window.blit(fond, (x_pos, y_pos))
                 x_pos += self.STEP
             y_pos += self.STEP
+
+    def show_path(self, path):
+        for case in path:
+            position = pygame.image.load("data/red_dot.png").convert_alpha()
+            self.window.blit(position, self.coord(case))
 
     def menu_loop(self):
         """
@@ -91,10 +133,15 @@ class laby_ui():
                     loop = False
                 self.window.blit(fond, (0, 0))
                 pygame.display.flip()
-
+    
+    def coord(self, case):
+        """return pixel postision from a grid coordonates"""
+        x_pos = case[0] * self.STEP
+        y_pos = case[1] * self.STEP
+        return (x_pos, y_pos)
 
 class Perso():
-
+    """Class that generate the perso"""
     PERSO = "data/perso.png"
     MOVES = {
         "0": (0, 0, 0, 0),
@@ -115,56 +162,71 @@ class Perso():
         "F": (1, 0, 1, 0)
     }
 
-    def __init__(self, window, step, laby):
+    def __init__(self, window, step, laby, move_left):
+        """init the perso moves with the GUI object
+        the step (nbr of pixel for 1 move)
+        laby is the labyrith code"""
         self.window = window
         self.step = step
         self.laby = laby
+        self.move_left = move_left
         # convert_alpha pour la gestion de la transparence
         self.perso = pygame.image.load(self.PERSO).convert_alpha()
         self.position_perso = self.perso.get_rect()
         self.window.blit(self.perso, self.position_perso)
 
     def authorized_move(self, direction):
-        x, y = self.return_position()
-        case = self.laby[y][x]
-        auth = self.MOVES[case]
-        print(x, y)
-        print(case)
-        for n in range(4):
+        """test if the destination is authorized"""
+        pos_x, pos_y = self.return_position()  # current perso position
+        case = self.laby[pos_y][pos_x]  # get current case code
+        auth = self.MOVES[case]  # translate code to walls positions
+        # print(pos_x, pos_y)
+        # print(case)
+        for n in range(4):  # test the 4 walls with the direction
             if direction[n] == 1 and auth[n] == 0:
+                self.move_left -= 1
                 return True
         return False
 
     def right(self):
+        """move the perso on the right case if possible"""
         if self.authorized_move((0, 1, 0, 0)):
             self.position_perso = self.position_perso.move(self.step, 0)
 
     def left(self):
+        """move the perso on the left case if possible"""
         if self.authorized_move((0, 0, 0, 1)):
             self.position_perso = self.position_perso.move(-self.step, 0)
 
     def up(self):
+        """move the perso on the upper case if possible"""
         if self.authorized_move((1, 0, 0, 0)):
             self.position_perso = self.position_perso.move(0, -self.step)
 
     def down(self):
+        """move the perso on the down case if possible"""
         if self.authorized_move((0, 0, 1, 0)):
             self.position_perso = self.position_perso.move(0, self.step)
 
     def load(self):
+        """refresh the new position"""
         self.window.blit(self.perso, self.position_perso)
+        return self.move_left
 
     def return_position(self):
+        """get a x,y position on the table"""
         pos_x = int(self.position_perso.left/self.step)
         pos_y = int(self.position_perso.top/self.step)
         return(pos_x, pos_y)
 
 
 def main():
-    game = laby_ui()
+    """Test function"""
+    game = LabyGUI()
     game.menu_loop()
-    labyrinth = Laby_gen()
-    game.game_loop(labyrinth.generate(21, 15))
+    labyrinth = LabyGenerator()
+    # game.game_loop(labyrinth.generate(21, 15))
+    game.game_loop(labyrinth.generate(10, 10))
 
 if __name__ == "__main__":
     main()
